b2bbe884d3e9fc2e9e81ab57b00c466998bfe7f1
Jonathan <jonverdao11@gmail.com>
2022-03-06 21:43:45 -0300

    update api

--- src/ListProduct/Api/api.js
+++ src/ListProduct/Api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'http://localhost/listproducts',
+    baseURL:'https://productlist43.000webhostapp.com/',
 });
 
 export default api;
